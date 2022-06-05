cdcb1b31c936854fe24657b60378d1cab16dbd88
Will <will@containerlabs.io>
2022-06-05 16:32:55 -0700

    using module monorepo

--- src/apis.tf
+++ src/apis.tf
@@ -1,4 +1,4 @@
 module "apis" {
-  source   = "github.com/massdriver-cloud/terraform-google-enable-apis"
+  source   = "github.com/massdriver-cloud/terraform-modules//google-enable-apis?ref=9201b9f"
   services = ["pubsub.googleapis.com"]
 }
--- src/monitoring.tf
+++ src/monitoring.tf
@@ -12,7 +12,7 @@ locals {
 
 module "topic_bytes_alarm" {
   count                          = var.monitoring_configuration.enabled ? 1 : 0
-  source                         = "github.com/massdriver-cloud/terraform-google-monitoring-utilization-threshold"
+  source                         = "github.com/massdriver-cloud/terraform-modules//google-monitoring-utilization-threshold?ref=9201b9f"
   md_metadata                    = var.md_metadata
   message                        = "PubSub Topic ${var.md_metadata.name_prefix} is above retainedBytes threshold of ${local.threshold_retained_bytes}"
   alarm_notification_channel_grn = var.subnetwork.data.observability.alarm_notification_channel_grn
