90244f753db5d7b397602fc2ff95b72c235cc68d
Will <will@containerlabs.io>
2022-06-09 21:36:20 -0700

    getting alarm channel id from metadata

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -15,7 +15,7 @@ module "topic_bytes_alarm" {
   source                         = "github.com/massdriver-cloud/terraform-modules//google-monitoring-utilization-threshold?ref=9201b9f"
   md_metadata                    = var.md_metadata
   message                        = "PubSub Topic ${var.md_metadata.name_prefix} is above retainedBytes threshold of ${local.threshold_retained_bytes}"
-  alarm_notification_channel_grn = var.subnetwork.data.observability.alarm_notification_channel_grn
+  alarm_notification_channel_grn = var.md_metadata.observability.alarm_channels.gcp.id
   alarm_name                     = "${google_pubsub_topic.main.id}-retainedBytes"
   metric_type                    = local.metrics["retained_bytes"].metric
   resource_type                  = local.metrics["retained_bytes"].resource
