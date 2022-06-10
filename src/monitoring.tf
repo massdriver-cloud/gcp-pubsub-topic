d395d7474f333199befaf24ebff3f2018c5f064b
Will <will@containerlabs.io>
2022-06-10 16:04:43 -0700

    updating bundle sha

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -11,17 +11,15 @@ locals {
 }
 
 module "topic_bytes_alarm" {
-  count                          = var.monitoring_configuration.enabled ? 1 : 0
-  source                         = "github.com/massdriver-cloud/terraform-modules//google-monitoring-utilization-threshold?ref=9201b9f"
-  md_metadata                    = var.md_metadata
-  message                        = "PubSub Topic ${var.md_metadata.name_prefix} is above retainedBytes threshold of ${local.threshold_retained_bytes}"
-  alarm_notification_channel_grn = var.md_metadata.observability.alarm_channels.gcp.id
-  alarm_name                     = "${google_pubsub_topic.main.id}-retainedBytes"
-  metric_type                    = local.metrics["retained_bytes"].metric
-  resource_type                  = local.metrics["retained_bytes"].resource
-  threshold                      = local.threshold_retained_bytes
-  period                         = 60
-  duration                       = 60
+  source        = "github.com/massdriver-cloud/terraform-modules//google-monitoring-utilization-threshold?ref=3ec7921"
+  md_metadata   = var.md_metadata
+  message       = "PubSub Topic ${var.md_metadata.name_prefix} is above retainedBytes threshold of ${local.threshold_retained_bytes}"
+  alarm_name    = "${google_pubsub_topic.main.id}-retainedBytes"
+  metric_type   = local.metrics["retained_bytes"].metric
+  resource_type = local.metrics["retained_bytes"].resource
+  threshold     = local.threshold_retained_bytes
+  period        = 60
+  duration      = 60
 
   depends_on = [
     google_pubsub_topic.main
