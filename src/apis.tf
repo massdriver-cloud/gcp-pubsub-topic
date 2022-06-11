599909ff591063009d1a200776a6d18fab1bea33
Will <will@containerlabs.io>
2022-06-11 11:34:44 -0700

    update module ref and path

--- src/apis.tf
+++ src/apis.tf
@@ -1,4 +1,4 @@
 module "apis" {
-  source   = "github.com/massdriver-cloud/terraform-modules//google-enable-apis?ref=9201b9f"
+  source   = "github.com/massdriver-cloud/terraform-modules//gcp-enable-apis?ref=c336d59"
   services = ["pubsub.googleapis.com"]
 }
--- src/monitoring.tf
+++ src/monitoring.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 module "topic_bytes_alarm" {
-  source        = "github.com/massdriver-cloud/terraform-modules//google-monitoring-utilization-threshold?ref=3ec7921"
+  source        = "github.com/massdriver-cloud/terraform-modules//gcp-monitoring-utilization-threshold?ref=3ec7921"
   md_metadata   = var.md_metadata
   message       = "PubSub Topic ${var.md_metadata.name_prefix} is above retainedBytes threshold of ${local.threshold_retained_bytes}"
   alarm_name    = "${google_pubsub_topic.main.id}-retainedBytes"
