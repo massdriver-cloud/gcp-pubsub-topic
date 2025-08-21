0ca4f1cf7ad3a4d482da67837d75cfc7d56baf8e
chrisghill <chris@massdriver.cloud>
2025-08-21 12:21:54 -0600

    remove deprecated field

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -28,8 +28,7 @@ locals {
 }
 
 resource "massdriver_artifact" "topic" {
-  field                = "topic"
-  provider_resource_id = google_pubsub_topic.main.id
-  name                 = "GCP PubSub Topic ${var.md_metadata.name_prefix} (${google_pubsub_topic.main.id})"
-  artifact             = jsonencode(local.artifact_topic)
+  field    = "topic"
+  name     = "GCP PubSub Topic ${var.md_metadata.name_prefix} (${google_pubsub_topic.main.id})"
+  artifact = jsonencode(local.artifact_topic)
 }
